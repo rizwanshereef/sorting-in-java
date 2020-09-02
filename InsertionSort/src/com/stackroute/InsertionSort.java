2d692cd85c5356f1a615d4c8ec6a95d2f1edbfb2
rsrizwan77 <rsrizwan77@gmail.com>
2020-09-02 11:15:23 +0530

    Adding InsertionSort

---
+++ InsertionSort/src/com/stackroute/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.stackroute;
+
+public class InsertionSort {
+/*Main method to implement Sorting*/
+    public static void main(String args[]) {
+        int[] num = { 12,54,64,23,7 };
+        insertionSort(num);
+        System.out.println("Sorted Array in Ascending Order: ");
+        for (int i=0; i<num.length;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+/*Method to sort the array*/
+    public static void insertionSort(int num[]) {
+        int len = num.length;
+        for (int i = 1; i < len; i++) {
+            int key = num[i];
+            int j = i - 1;
+
+            while (j >= 0 && key < num[j]) {
+                num[j + 1] = num[j];
+                --j;
+            }
+            num[j + 1] = key;
+        }
+    }
+
+}
